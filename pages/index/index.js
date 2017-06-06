b7b76801039eec7753adf31de3266bb3fe0368b0
kunkuntang <kuntang@163.com>
2017-06-06 08:15:31 +0800

    store

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,93 +1,90 @@
-var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: {
-      nickName: 'xingkongus',
-      avatarUrl: '../../images/home.png'
-    },
-    majorArray: [
-      ['软件', '计科', '网络', '信息'],
-      ['国商', '英语']
-    ],
-    collegeArray: ['计算机', '外国语'],
-    collegeIdx: 0,
-    majorIdx: 0,
-    date: '2016-09-01',
-    time: '12:01'
+    bookList: [
+      {
+        bookPic: '../../images/home.png',
+        bookName: 'js',
+        bookPrice: 100,
+        bookDiscount: 0.7
+      },
+      {
+        bookPic: '../../images/home.png',
+        bookName: 'css',
+        bookPrice: 100,
+        bookDiscount: 0.7
+      },
+      {
+        bookPic: '../../images/home.png',
+        bookName: 'nodejs',
+        bookPrice: 100,
+        bookDiscount: 0.7
+      },
+      {
+        bookPic: '../../images/home.png',
+        bookName: 'html',
+        bookPrice: 100,
+        bookDiscount: 0.7
+      }
+    ]
   },
-  bindSelectCollege: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      collegeIdx: e.detail.value,
-      majorIdx: 0
-    })
-  },
-  bindSelectMajor: function (e) {
-    console.log('bindSelectMajor value', e.detail.value)
-    this.setData({
-      majorIdx: e.detail.value
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    app.getUserInfo(function (userInfo) {
-      that.setData({ userInfo: userInfo })
-    });
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,21 +1,48 @@
-//login.js
-var that;
-
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {
+      nickName: 'xingkongus',
+      avatarUrl: '../../images/home.png'
+    },
+    majorArray: [
+      ['软件', '计科', '网络', '信息'],
+      ['国商', '英语']
+    ],
+    collegeArray: ['计算机', '外国语'],
+    collegeIdx: 0,
+    majorIdx: 0
   },
-
+  bindSelectCollege: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      collegeIdx: e.detail.value,
+      majorIdx: 0
+    })
+  },
+  bindSelectMajor: function (e) {
+    console.log('bindSelectMajor value', e.detail.value)
+    this.setData({
+      majorIdx: e.detail.value
+    })
+  },
+  login: function (e) {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    that = this;
-    
+    var that = this;
+    app.getUserInfo(function (userInfo) {
+      that.setData({ userInfo: userInfo })
+    });
   },
 
   /**
@@ -65,24 +92,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  
-  globalData: {
-    userInfo: null
   }
 })
