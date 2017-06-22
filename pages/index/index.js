cbfee6d018271a038c2b4e0af69ffbc17ab5b1f4
Tomzi_Max <kuntang@Tomzi-Maxs-MacBook-Pro.local>
2017-06-22 21:12:15 +0800

    add the add book func

--- pages/index/index.js
+++ pages/index/index.js
@@ -38,71 +38,84 @@ Page({
         isSelected: false
       }
     ],
-    selected: []
+    selected: [],
+    sumPrice: 0
   },
   selectBook: function (e) {
     var idx = e.currentTarget.dataset.index
-    console.log(idx)
     var tempData = this.data.bookList
-    tempData[idx].isSelected = !this.data.bookList[idx].isSelected
+    var bookId = tempData[idx].bookId
+    var isBuy = tempData[idx].isSelected = !this.data.bookList[idx].isSelected
 
+    var tempSelected = this.data.selected
+    var tempSumPrice = this.data.sumPrice
+    if (isBuy) {
+      tempSelected.push(bookId)
+      tempSumPrice += tempData[idx].bookPrice
+    } else {
+      tempSumPrice -= tempData[idx].bookPrice
+      tempSelected.splice(tempSelected.indexOf(bookId), 1)
+    }
     this.setData({
-      bookList: tempData
+      bookList: tempData,
+      selected: tempSelected,
+      sumPrice: tempSumPrice
     })
+    console.log(tempSelected)
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
