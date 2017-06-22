4192f15bce76a7ea94ba6d6e3a6cd626744b53dd
kunkuntang <kuntang@163.com>
2017-06-22 11:29:25 +0800

    finished the step and the bookSelect func

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,32 +6,50 @@ Page({
   data: {
     bookList: [
       {
+        bookId: 0,
         bookPic: '../../images/home.png',
         bookName: 'js',
         bookPrice: 100,
-        bookDiscount: 0.7
+        bookDiscount: 0.7,
+        isSelected: false
       },
       {
+        bookId: 1,
         bookPic: '../../images/home.png',
         bookName: 'css',
         bookPrice: 100,
-        bookDiscount: 0.7
+        bookDiscount: 0.7,
+        isSelected: false
       },
       {
+        bookId: 2,
         bookPic: '../../images/home.png',
         bookName: 'nodejs',
         bookPrice: 100,
-        bookDiscount: 0.7
+        bookDiscount: 0.7,
+        isSelected: false
       },
       {
+        bookId: 3,
         bookPic: '../../images/home.png',
         bookName: 'html',
         bookPrice: 100,
-        bookDiscount: 0.7
+        bookDiscount: 0.7,
+        isSelected: false
       }
-    ]
+    ],
+    selected: []
   },
+  selectBook: function (e) {
+    var idx = e.currentTarget.dataset.index
+    console.log(idx)
+    var tempData = this.data.bookList
+    tempData[idx].isSelected = !this.data.bookList[idx].isSelected
 
+    this.setData({
+      bookList: tempData
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- pages/login/login.js
+++ pages/login/login.js
@@ -15,21 +15,47 @@ function _next() {
   }, 20);
 }
 
+function _pre() {
+  var that = this;
+  var speed = 1;
+  if (this.data.progress <= this.data.stepArr[this.data.step]) {
+    return true
+  }
+  this.setData({
+    progress: this.data.progress - speed
+  });
+  setTimeout(function () {
+    _pre.call(that);
+  }, 20);
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    stepArr: ['', 33.3, 66.6, 100],
     step: 0,
+    stepArr: [0, 50, 100],
+    pageArr: ['codeInput', 'baseInfo', 'phoneInput'],
     progress: 0,
     userInfo: {
       nickName: 'xingkongus',
       avatarUrl: '../../images/home.png'
     }
   },
+  pre: function (e) {
+    this.setData({
+      step: --this.data.step
+    })
+    _pre.call(this)
+  },
   next: function (e) {
+    if (this.data.step === 2) {
+      wx.navigateTo({
+        url: '/pages/index/index',
+      })
+    }
     this.setData({
       step: ++this.data.step
     })
